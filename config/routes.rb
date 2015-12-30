3bf9a4408042564dff7416479c35441bc2cd8f1d
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2015-12-30 14:16:56 +0100

    About route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'app#index'
 
+  get 'o-webove-typografii' => 'app#about'
+
   post 'app/index' => 'app#index'
   post '/' => 'app#index'
 
